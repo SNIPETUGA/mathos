75977f4c0dde1110859a1e06d1a1c0a8041e2432
Bernardo Santos <bernardosantos@MacBook-Pro-de-Bernardo.local>
2026-04-30 10:57:38 +0100

    prepare for deployment

--- app.py
+++ app.py
@@ -120,4 +120,5 @@ def get_by_person(person):
 setup()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
